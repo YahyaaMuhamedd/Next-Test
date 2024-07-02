64ebd1dd336ef289dd232d3f2452d610966e8686
MOOS <144733181+YahyaaMuhamedd@users.noreply.github.com>
2024-07-02 16:20:04 +0300

    up

--- src/app/component/slider.jsx
+++ src/app/component/slider.jsx
@@ -33,10 +33,10 @@ export default function Slider() {
 className='mt-1 h-75vh w-100'
                 >
 
-                    <SwiperSlide><Image src={logo.src} alt="asda" width={1200} height={500}  className='slide-img'/></SwiperSlide>
-                    <SwiperSlide><Image src={logo1.src} alt="asda" width={1200} height={500} className='slide-img' /></SwiperSlide>
-                    <SwiperSlide><Image src={logo2.src} alt="asda" width={1200} height={500} className='slide-img' /></SwiperSlide>
-                    <SwiperSlide><Image src={logo.src} alt="asda" width={1200} height={500}  className='slide-img'/></SwiperSlide>
+                    <SwiperSlide><Image src={logo.src} alt="asda" width={1340} height={500}  /></SwiperSlide>
+                    <SwiperSlide><Image src={logo1.src} alt="asda" width={1340} height={500}  /></SwiperSlide>
+                    <SwiperSlide><Image src={logo2.src} alt="asda" width={1340} height={500}  /></SwiperSlide>
+                    <SwiperSlide><Image src={logo.src} alt="asda" width={1340} height={500}  /></SwiperSlide>
 
 
                 </Swiper>
