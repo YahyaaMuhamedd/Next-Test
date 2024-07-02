beade2b1ed3a708a58236ee7a0163193ccffaeea
MOOS <144733181+YahyaaMuhamedd@users.noreply.github.com>
2024-07-02 15:56:17 +0300

    second

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -2,12 +2,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { delay, motion } from 'framer-motion';
+
 
 
 export default function Products() {
     const [allProducts, setAllProducts] = useState([]);
- 
-    
+
+
 
     useEffect(() => {
         fetchProducts();
@@ -24,26 +26,58 @@ export default function Products() {
         }
     };
 
-   
+
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2
+            }
+        }
+    };
+
+    const item = {
+        hidden: { y: 200, opacity: 0, duration: 5 },
+        visible: {
+            y: 0,
+            opacity: 1,
+            delay: 10,
+
+        }
+    };
 
     return (
         <>
-            <div className="row container m-auto mt-5">
-                <h1 className='text-center text-white'>Products</h1>
-                {allProducts.map((product) => (
-                    <div key={product.id} className="col-sm-12 col-md-6 col-lg-4 mb-4">
-                        <div className="card h-100">
-                            <Image src={product.image} className="card-img-top" height={400} width={300} alt={product.title} />
-                            <div className="card-body">
-                                <h5 className="card-title">{product.title}</h5>
-                                <p className="card-text">${product.price}</p>
-                                <Link href={`/products/${product.id}`} className="btn btn-primary">View Details</Link>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            
+            <motion.ul
+                className="container"
+                variants={item}
+                initial="hidden"
+                animate="visible"
+            >
+                <div className="row container m-auto mt-5">
+                    <h1 className='text-center text-white'>Products</h1>
+                    {allProducts.map((product) => (
+                        <motion.div key={product.id} className="col-sm-12 col-md-6 col-lg-4 mb-4"
+
+                            variants={container}
+                            initial="hidden"
+                            animate="visible">
+                            <motion.div className="card h-100" key={index}>
+                                <Image src={product.image} className="card-img-top" height={400} width={300} alt={product.title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.title}</h5>
+                                    <p className="card-text">${product.price}</p>
+                                    <Link href={`/products/${product.id}`} className="btn btn-primary">View Details</Link>
+                                </div>
+                            </motion.div>
+                        </motion.div>
+                    ))}
+                </div>
+            </motion.ul>
+
         </>
     );
 }
